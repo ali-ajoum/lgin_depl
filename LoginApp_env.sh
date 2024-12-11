e7cca976fa3c679767351f32f23cab840e8c0eaf
NetAcad DEVASC <changeme@example.com>
2024-12-11 10:58:03 +0000

    first commit

---
+++ LoginApp_env.sh
@@ -0,0 +1,17 @@
+mkdir weather_project
+cd weather_project
+
+python3 -m venv loginapp_env
+
+source loginapp_env/bin/activate
+
+# pip install requests
+# pip show requests
+
+#$ pip list
+#$ pip uninstall Flask
+### SOURCE
+#$ pip freeze > requirements.txt
+### TARGET
+#$ pip install -r requirements.txt
+
